358bbfcfdcb31809057ebd01fec3c174c3b7131f
SystematicError <systematicerror@users.noreply.github.com>
2022-08-18 10:22:58 +0000

    Add Lua 5.1 / LuaJIT compatibility

--- timed.lua
+++ timed.lua
@@ -291,7 +291,7 @@ local function timed(args)
 	end
 
 	--override to allow calling fire with no arguments
-	function obj:fire(...) args = ({...})[1] and {...} or {obj.pos, obj._time, obj._dt}; for _, func in pairs(obj._subscribed) do func(table.unpack(args)) end end
+	function obj:fire(...) args = ({...})[1] and {...} or {obj.pos, obj._time, obj._dt}; for _, func in pairs(obj._subscribed) do func((table.unpack or unpack)(args)) end end
 
 	--subscribe stuff initially and add callback
 	obj.subscribe_callback = function(func) func(obj.pos, obj._time, obj._dt) end
